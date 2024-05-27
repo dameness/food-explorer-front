357df84c814ee95e5bf3185911f0e822c8addfad
bernardo damiani <bmdamiani17@gmail.com>
2024-05-27 14:42:16 -0300

    style: add root layout styles

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { Outlet } from 'react-router-dom';
 import { ThemeContext } from '@/contexts/themeContext';
 import { Header } from './header';
+import { Footer } from './footer';
 
 export const RootLayout = () => {
   const { theme } = useContext(ThemeContext);
@@ -9,9 +10,10 @@ export const RootLayout = () => {
     <div className={theme}>
       <div className='dark:text-neutral-200 text-neutral-700'>
         <Header />
-        <main className='min-w-full'>
+        <main className='flex w-[96%] max-w-7xl mx-auto h-[calc(100vh - 160px)] flex-col items-center'>
           <Outlet />
         </main>
+        <Footer />
       </div>
     </div>
   );
