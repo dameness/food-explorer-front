ffda5b6aee46ee0ff3b21b015b2463b65ce020be
bernardo damiani <bmdamiani17@gmail.com>
2024-05-27 14:18:36 -0300

    feat: add layout header

---
+++ src/components/header.tsx
@@ -0,0 +1,39 @@
+import { ThemeContext } from '@/contexts/themeContext';
+import {
+  ChefHat,
+  Hexagon,
+  LogOut,
+  Menu,
+  Moon,
+  Search,
+  Sun,
+} from 'lucide-react';
+import { useContext } from 'react';
+export const Header = () => {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  return (
+    <header className='flex justify-between items-center p-4 h-24 w-full dark:bg-slate-900'>
+      <div className='flex items-center gap-2'>
+        <Menu size={28} />
+        <button onClick={toggleTheme}>
+          {theme === 'light' ? <Sun size={28} /> : <Moon size={28} />}
+        </button>
+      </div>
+      <div className='flex items-center gap-1 font-semibold sm:text-xl text-lg'>
+        <Hexagon className='text-blue-400 opacity-60' fill='#60a5fa' />
+        food explorer
+      </div>
+      <div className='sm:flex classname w-1/2 items-center hidden border border-slate-200/60 py-1.5 px-2 gap-1 rounded-lg'>
+        <Search size={16} className='opacity-60' />
+        <input
+          className='bg-transparent text-sm'
+          placeholder='Search for dishes or ingredients'
+        />
+      </div>
+      <div className='flex items-center gap-2'>
+        <ChefHat size={28} />
+        <LogOut className='sm:block hidden' size={28} />
+      </div>
+    </header>
+  );
+};
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,18 @@
 import { useContext } from 'react';
 import { Outlet } from 'react-router-dom';
-import { ThemeContext } from '../contexts/themeContext';
+import { ThemeContext } from '@/contexts/themeContext';
+import { Header } from './header';
 
 export const RootLayout = () => {
   const { theme } = useContext(ThemeContext);
   return (
     <div className={theme}>
-      <Outlet />
+      <div className='dark:text-neutral-200 text-neutral-700'>
+        <Header />
+        <main className='min-w-full'>
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 };
