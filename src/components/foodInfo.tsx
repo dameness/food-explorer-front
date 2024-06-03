70b58c1b8f09aea0008a26a225e18e13948fcb94
bernardo damiani <bmdamiani17@gmail.com>
2024-06-03 15:56:01 -0300

    refactor: changes foodInfo layout

--- src/components/foodInfo.tsx
+++ src/components/foodInfo.tsx
@@ -8,39 +8,36 @@ interface FoodProps {
 
 export const FoodInfo = ({ data }: FoodProps) => {
   return (
-    <>
-      <img
-        src='/assets/mask-group.png'
-        className='sm:w-48 xs:w-36 w-24'
-        alt='food'
-      />
+    <div className='flex sm:flex-row flex-col gap-4 items-center justify-center'>
+      <img src='/assets/mask-group.png' className='w-56' alt='food' />
 
-      <h1>{data.name}</h1>
-      <h1>{data.description}</h1>
-      <div className='grid grid-cols-3 gap-3 w-11/12 max-w-72'>
-        {data.ingredients.map((ingredient) => (
-          <div
-            key={ingredient}
-            className='text-center xs:text-sm text-xs text-ellipsis overflow-hidden bg-Dark/Dark-1000 px-2 py-1.5 rounded-md'
-          >
-            {ingredient}
-          </div>
-        ))}
-      </div>
-
-      <h1 className='text-2xl'></h1>
-
-      <div className='flex items-center'>
-        <div className='flex items-center gap-1'>
-          <Minus />
-          01
-          <Plus />
+      <div className='flex flex-col gap-4 items-center justify-center'>
+        <h1 className='sm:self-start self-auto text-2xl font-medium'>
+          {data.name}
+        </h1>
+        <h1 className='sm:self-start self-auto text-sm'>{data.description}</h1>
+        <div className='grid sm:grid-cols-6 grid-cols-3 gap-3 w-11/12 sm:max-w-full max-w-72 '>
+          {data.ingredients.map((ingredient) => (
+            <div
+              key={ingredient}
+              className=' font-medium text-center xs:text-sm text-xs text-ellipsis overflow-hidden bg-Dark/Dark-1000 px-2 py-1.5 rounded-md'
+            >
+              {ingredient}
+            </div>
+          ))}
         </div>
-        <Button>
-          <ReceiptText />
-          order - {'$ ' + data.price.toFixed(2)}
-        </Button>
+        <div className='sm:self-start self-auto flex items-center gap-3'>
+          <div className='flex items-center gap-2 font-semibold text-lg'>
+            <Minus />
+            01
+            <Plus />
+          </div>
+          <Button>
+            <ReceiptText />
+            order - {'$ ' + data.price.toFixed(2)}
+          </Button>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
