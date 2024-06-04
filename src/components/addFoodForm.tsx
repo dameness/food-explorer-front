256c859f1320ecacd5978911490772a46e8a2af0
bernardo damiani <bmdamiani17@gmail.com>
2024-06-04 15:55:17 -0300

    style: add form fields

--- src/components/addFoodForm.tsx
+++ src/components/addFoodForm.tsx
@@ -1,16 +1,24 @@
+import { Button } from './ui/button';
 import { Input } from './ui/input';
 import { Select } from './ui/select';
 import { TextArea } from './ui/textarea';
 
 export const AddFoodForm = () => {
   return (
-    <form>
-      <Input />
-      <Input />
-      <Select />
-      <Input />
-      <Input />
-      <TextArea />
+    <form className='w-full flex flex-col gap-2 [&>input]:bg-Dark/Dark-800'>
+      <label htmlFor='image'> Image</label>
+      <Input id='image' />
+      <label htmlFor='name'> Name</label>
+      <Input id='name' />
+      <label htmlFor='category'> Category</label>
+      <Select id='category' />
+      <label htmlFor='ingredients'> Ingredients</label>
+      <Input id='ingredients' />
+      <label htmlFor='price'> Price</label>
+      <Input id='price' />
+      <label htmlFor='description'> Description</label>
+      <TextArea id='description' />
+      <Button className='bg-Tints/Tomato-400' />
     </form>
   );
 };
