f4b5f4810d6fb201d09cfa2d92c4008b81d534ec
bernardo damiani <bmdamiani17@gmail.com>
2024-06-04 11:09:41 -0300

    style: finish food info styles

--- src/components/foodInfo.tsx
+++ src/components/foodInfo.tsx
@@ -8,27 +8,31 @@ interface FoodProps {
 
 export const FoodInfo = ({ data }: FoodProps) => {
   return (
-    <div className='h-[calc(100vh-240px)] flex md:flex-row flex-col gap-4 items-center justify-center'>
-      <img src='/assets/mask-group.png' className='md:w-80 w-56' alt='food' />
+    <div className='flex md:flex-row flex-col gap-4 items-center justify-center'>
+      <img
+        src='/assets/mask-group.png'
+        className='lg:w-72 md:w-64 w-56'
+        alt='food'
+      />
 
       <div className='flex flex-col md:gap-4 gap-8 items-center justify-center px-8'>
         <h1 className='md:self-start self-auto text-3xl font-medium'>
           {data.name}
         </h1>
-        <h1 className='md:self-start self-auto md:text-lg text-md'>
+        <h1 className='md:self-start self-auto md:text-lg text-md text-justify'>
           {data.description}
         </h1>
-        <div className='grid lg:grid-cols-6 grid-cols-3 gap-3 w-full'>
+        <div className='grid lg:grid-cols-6 grid-cols-3 gap-x-6 gap-y-4 w-full'>
           {data.ingredients.map((ingredient) => (
             <div
               key={ingredient}
-              className=' font-medium text-center text-sm text-ellipsis overflow-hidden bg-Dark/Dark-1000 px-2 py-1.5 rounded-md'
+              className=' font-medium text-center text-sm truncate bg-Dark/Dark-1000 px-2 py-1.5 rounded-md'
             >
               {ingredient}
             </div>
           ))}
         </div>
-        <div className='w-full md:self-start self-auto flex items-center md:justify-start justify-between gap-6'>
+        <div className='md:mt-4 w-full md:self-start self-auto flex items-center md:justify-start justify-between gap-6'>
           <div className='flex items-center gap-2 font-semibold text-lg'>
             <Minus />
             01
--- src/pages/food.tsx
+++ src/pages/food.tsx
@@ -5,21 +5,31 @@ import { Link } from 'react-router-dom';
 export const Food = () => {
   return (
     <>
-      <Link to='/' className='mt-4 flex items-center place-self-start'>
-        <ChevronLeft />
+      <Link
+        to='/'
+        className='text-lg font-semibold mt-4 flex items-center place-self-start'
+      >
+        <ChevronLeft size={32} />
         return
       </Link>
 
       <FoodInfo
         data={{
           name: 'Caesar Salad',
-          description: 'This is the food description',
+          description:
+            'A Caesar salad is a green salad of romaine lettuce and croutons dressed with lemon juice (or lime juice), olive oil, eggs or egg yolks, Worcestershire sauce, anchovies, garlic, Dijon mustard, Parmesan cheese, and black pepper.',
           ingredients: [
-            'ingredient1312312312312',
-            'ingre2',
-            'ingredient3',
-            'i4',
-            'ingredient5',
+            'lettuce',
+            'croutons',
+            'lemon juice',
+            'olive oil',
+            'eggs',
+            'worcestershire sauce',
+            'anchovies',
+            'garlic',
+            'dijon mustard',
+            'parmesan',
+            'black pepper',
           ],
           price: 9.99,
         }}
